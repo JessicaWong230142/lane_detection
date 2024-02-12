6db076fb1b2ded2965f6443b7932729643b77b04
JessicaWong230142 <113719950+JessicaWong230142@users.noreply.github.com>
2024-02-12 02:30:43 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+from lane_detection import detect_lines
+
+#takes in video feed from camera
+video_feed = cv2.VideoCapture(0)
+
+#continuosly capture frames from video_feed
+while True:
+
+    #reads frames
+    ret, frame = video_feed.read()
+
+    #calls detect_lines function and applies to frame
+    updated_frame_with_lines = detect_lines(frame)
+
+    #resizes frame to fit window
+    display_frame = cv2.resize(updated_frame_with_lines, dsize=(2000, 1200))
+
+    #creates window to display lines and mask on frame
+    cv2.imshow('lane detection', display_frame)
+
+    #ends program if q is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#releases video_feed and closes opencv windows
+video_feed.release()
+cv2.destroyAllWindows()
